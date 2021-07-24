5dbca361647e96d2faa8f514a4bdd6f98fa5fcb0
jjjack1 <86045983+jjjack1@users.noreply.github.com>
2021-07-24 21:08:12 +0900

    Update ui_building.js

--- public/js/ui_building.js
+++ public/js/ui_building.js
@@ -86,7 +86,7 @@ function build_user_panels(data) {
 
 		let disableHtml = '';
 		if (data[i].company  === escapeHtml(bag.marble_company)) {
-			disableHtml = '<span style="color: red; font-size:18px;"><span class="fa fa-rocket fa-5x disableOwner" title="대출실행">대출실행</span></span>';
+			disableHtml = '<span style="color: red; font-size:25px;"><span class="fa fa-rocket fa-5x disableOwner" title="대출실행">대출실행</span></span>';
 		}
 
 		html += `<div id="user` + i + `wrap" username="` + data[i].username + `" company="` + data[i].company +
