26536092889aea0974f527f2f1969ac7b5e57976
jjjack1 <86045983+jjjack1@users.noreply.github.com>
2021-07-24 20:48:07 +0900

    Update ui_building.js

--- public/js/ui_building.js
+++ public/js/ui_building.js
@@ -86,7 +86,7 @@ function build_user_panels(data) {
 
 		let disableHtml = '';
 		if (data[i].company  === escapeHtml(bag.marble_company)) {
-			disableHtml = '<span class="fa fa-rocket fa-5x disableOwner" title="Disable Owner"></span>';
+			disableHtml = '<span class="fa fa-rocket disableOwner fa-lg" title="Disable Owner"></span>';
 		}
 
 		html += `<div id="user` + i + `wrap" username="` + data[i].username + `" company="` + data[i].company +
