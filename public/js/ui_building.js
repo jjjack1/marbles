d5d27f06753b2c31bc8d1f3e2ba25d9403eaddef
jjjack1 <86045983+jjjack1@users.noreply.github.com>
2021-07-24 21:04:49 +0900

    Update ui_building.js

--- public/js/ui_building.js
+++ public/js/ui_building.js
@@ -93,7 +93,7 @@ function build_user_panels(data) {
 			`" owner_id="` + data[i].id + `" class="marblesWrap ` + colorClass + `">
 					<div class="legend" style="` + size_user_name(data[i].username) + `">
 						` + toTitleCase(data[i].username) + `
-						<span class="fa fa-thumb-tack marblesFix" title="Never Hide Owner"></span>
+						
 						` + disableHtml + `
 					</div>
 					<div class="innerMarbleWrap"><i class="fa fa-plus addMarble"></i></div>
